f72d3e86c529746dbde9e13a81cc0da2affc0836
bruno20614 <brunocdl9@gmail.com>
2024-08-07 14:53:20 -0300

    Criação do programa de geração de senhas

--- main.go
+++ main.go
@@ -1,11 +1,33 @@
 package main
 
 import (
-	"fmt"
 	"crypto/rand"
+	"fmt"
 	"math/big"
 )
 
 const (
-	letters =
+	letters = "abcdefghijklmnopqrstuvwxyz"
+	digits  = "0123456789"
+	symbols = "!@#$%^&*()-_=+[]{}<>?/|\\"
 )
+
+func main() {
+	var length int
+	fmt.Println("Write the password length")
+	fmt.Scan(&length)
+
+	password := generatePassword(length)
+	fmt.Println("Password Created", password)
+}
+
+func generatePassword(length int) string {
+	charset := letters + digits + symbols
+	password := make([]byte, length)
+
+	for i := range password {
+		randomChar, _ := rand.Int(rand.Reader, big.NewInt(int64(len(charset))))
+		password[i] = charset[randomChar.Int64()]
+	}
+	return string(password)
+}
